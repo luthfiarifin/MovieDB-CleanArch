1c4fe6eaf7e7b948ad474e4005fcc3cc1a91568d
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-10-23 04:09:37 +0700

    Configure navigation in MainActivity.kt

--- app/src/main/java/com/laam/moviedb_cleanarch/presentation/main/MainActivity.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/presentation/main/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.laam.moviedb_cleanarch.presentation.main
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.laam.moviedb_cleanarch.R
 import com.laam.moviedb_cleanarch.databinding.ActivityMainBinding
 import com.laam.moviedb_cleanarch.presentation.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
-    override fun getLayoutId(): Int  = R.layout.activity_main
+    override fun getLayoutId(): Int = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+
+        navHostFragment.navController.let { navController ->
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                if (destination.id == R.id.splashFragment) supportActionBar?.hide()
+                else supportActionBar?.show()
+            }
+        }
     }
 }
