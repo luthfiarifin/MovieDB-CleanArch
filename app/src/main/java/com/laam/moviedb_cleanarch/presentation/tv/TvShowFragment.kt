7d33245b1f2674d5a56de3297a58a14d8c787c72
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-10-26 05:54:48 +0700

    Configure TvShowFragment.kt

--- app/src/main/java/com/laam/moviedb_cleanarch/presentation/tv/TvShowFragment.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/presentation/tv/TvShowFragment.kt
@@ -6,14 +6,28 @@ import com.laam.moviedb_cleanarch.R
 import com.laam.moviedb_cleanarch.databinding.FragmentTvBinding
 import com.laam.moviedb_cleanarch.presentation.base.BaseFragment
 
-class TvFragment : BaseFragment<FragmentTvBinding, TvViewModel>() {
+class TvShowFragment : BaseFragment<FragmentTvBinding, TvViewModel>() {
 
     override fun getLayoutId(): Int = R.layout.fragment_tv
 
     override fun getViewModel(): Class<TvViewModel> = TvViewModel::class.java
 
+    private val rvAdapter = TvShowRecyclerAdapter()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUpRvAdapter()
+        setUpRecycler()
+    }
+
+    private fun setUpRvAdapter() {
+        rvAdapter.submitList(viewModel.tvShowList)
+    }
+
+    private fun setUpRecycler() {
+        with(viewBinding.rvTvShow) {
+            adapter = rvAdapter
+        }
     }
 }
--- app/src/main/java/com/laam/moviedb_cleanarch/presentation/tv/TvViewModel.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/presentation/tv/TvViewModel.kt
@@ -3,4 +3,8 @@ package com.laam.moviedb_cleanarch.presentation.tv
 import com.laam.moviedb_cleanarch.presentation.base.BaseViewModel
 import javax.inject.Inject
 
-class TvViewModel @Inject constructor() : BaseViewModel()
+class TvViewModel @Inject constructor(private val interactors: TvShowListInteractors) :
+    BaseViewModel() {
+
+    val tvShowList = interactors.getAllTvShows.invoke()
+}
