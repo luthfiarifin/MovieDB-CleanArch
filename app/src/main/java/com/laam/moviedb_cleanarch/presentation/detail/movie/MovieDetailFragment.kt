2ec1ebfa7445b1c5f933020049190594feac0bcd
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-10-27 17:55:54 +0700

    Change navigation icon & Add share menu icon

--- app/src/main/java/com/laam/moviedb_cleanarch/presentation/detail/movie/MovieDetailFragment.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/presentation/detail/movie/MovieDetailFragment.kt
@@ -1,10 +1,13 @@
 package com.laam.moviedb_cleanarch.presentation.detail.movie
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import com.laam.moviedb_cleanarch.R
 import com.laam.moviedb_cleanarch.databinding.FragmentMovieDetailBinding
 import com.laam.moviedb_cleanarch.presentation.base.BaseFragment
+import com.laam.moviedb_cleanarch.presentation.main.MainActivity
 
 class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding, MovieDetailViewModel>() {
 
@@ -15,8 +18,17 @@ class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding, MovieDetail
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUpToolbar()
         setUpBinding()
         setUpViewModelVariable()
+    }
+
+    private fun setUpToolbar() {
+        (activity as MainActivity).apply {
+            setSupportActionBar(this@MovieDetailFragment.viewBinding.toolbar)
+            title = ""
+        }
+        setHasOptionsMenu(true)
     }
 
     private fun setUpViewModelVariable() {
@@ -30,5 +42,11 @@ class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding, MovieDetail
         viewBinding.toolbar.setNavigationOnClickListener {
             activity?.onBackPressed()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_share, menu)
     }
 }
--- app/src/main/java/com/laam/moviedb_cleanarch/presentation/detail/tvshow/TvShowDetailFragment.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/presentation/detail/tvshow/TvShowDetailFragment.kt
@@ -1,10 +1,13 @@
 package com.laam.moviedb_cleanarch.presentation.detail.tvshow
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import com.laam.moviedb_cleanarch.R
 import com.laam.moviedb_cleanarch.databinding.FragmentTvshowDetailBinding
 import com.laam.moviedb_cleanarch.presentation.base.BaseFragment
+import com.laam.moviedb_cleanarch.presentation.main.MainActivity
 
 class TvShowDetailFragment : BaseFragment<FragmentTvshowDetailBinding, TvShowDetailViewModel>() {
 
@@ -15,8 +18,17 @@ class TvShowDetailFragment : BaseFragment<FragmentTvshowDetailBinding, TvShowDet
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUpToolbar()
         setUpBinding()
         setUpViewModelVariable()
+    }
+
+    private fun setUpToolbar() {
+        (activity as MainActivity).apply {
+            setSupportActionBar(this@TvShowDetailFragment.viewBinding.toolbar)
+            title = ""
+        }
+        setHasOptionsMenu(true)
     }
 
     private fun setUpViewModelVariable() {
@@ -30,5 +42,11 @@ class TvShowDetailFragment : BaseFragment<FragmentTvshowDetailBinding, TvShowDet
         viewBinding.toolbar.setNavigationOnClickListener {
             activity?.onBackPressed()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_share, menu)
     }
 }
