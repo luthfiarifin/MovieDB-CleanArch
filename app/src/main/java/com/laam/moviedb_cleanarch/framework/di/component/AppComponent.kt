3898a0dfe11e7031487a211db70e2d458d928fb4
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-10-30 16:24:20 +0700

    Configure AppComponent.kt to Component of Dagger

---
+++ app/src/main/java/com/laam/moviedb_cleanarch/framework/di/component/AppComponent.kt
@@ -0,0 +1,33 @@
+package com.laam.moviedb_cleanarch.framework.di.component
+
+import android.app.Application
+import com.laam.moviedb_cleanarch.framework.di.builder.ActivityBuilder
+import com.laam.moviedb_cleanarch.framework.di.module.ViewModelModule
+import com.laam.moviedb_cleanarch.framework.di.module.ViewModelProviderFactoryModule
+import com.laam.moviedb_cleanarch.presentation.MovieApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        ActivityBuilder::class,
+        ViewModelProviderFactoryModule::class,
+        ViewModelModule::class
+    ]
+)
+interface AppComponent : AndroidInjector<MovieApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+}
