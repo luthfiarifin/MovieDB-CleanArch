074ebb73db073182f3d3905720366291e5701a30
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-10-30 16:37:08 +0700

    Add constructor injection on data source implementation

--- app/src/main/java/com/laam/moviedb_cleanarch/framework/datasource/TvShowDataSourceImpl.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/framework/datasource/TvShowDataSourceImpl.kt
@@ -3,8 +3,9 @@ package com.laam.moviedb_cleanarch.framework.datasource
 import com.laam.core.model.TvShow
 import com.laam.core.repository.tv.TvShowDataSource
 import com.laam.moviedb_cleanarch.framework.dummy.TvShowDummy
+import javax.inject.Inject
 
-class TvShowDataSourceImpl : TvShowDataSource {
+class TvShowDataSourceImpl @Inject constructor() : TvShowDataSource {
 
     override fun getAll(): List<TvShow> = TvShowDummy.generateDummyTvShow()
 
