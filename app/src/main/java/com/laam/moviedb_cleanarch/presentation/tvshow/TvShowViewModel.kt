ed1f5c5de2f5957c90a8f4c07863b0774624b519
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-11-06 09:56:38 +0700

    Reconfigure TvShowFragment to get data from API and add loading state

--- app/src/main/java/com/laam/moviedb_cleanarch/presentation/tvshow/TvShowViewModel.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/presentation/tvshow/TvShowViewModel.kt
@@ -1,13 +1,42 @@
 package com.laam.moviedb_cleanarch.presentation.tvshow
 
 import androidx.databinding.ObservableBoolean
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.laam.core.ext.repository.State
+import com.laam.core.model.MoviePagination
+import com.laam.core.model.TvShow
 import com.laam.moviedb_cleanarch.presentation.base.BaseViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class TvShowViewModel @Inject constructor(
-    interactors: TvShowListInteractors
+    private val interactors: TvShowListInteractors
 ) : BaseViewModel() {
 
+    val isLoading: ObservableBoolean = ObservableBoolean(false)
     val isEmptyData: ObservableBoolean = ObservableBoolean(false)
-    val tvShowList = interactors.getAllTvShows.invoke()
+
+    private val _tvShowsLiveData = MutableLiveData<State<MoviePagination<TvShow>>>()
+    val tvShowsLiveData: LiveData<State<MoviePagination<TvShow>>>
+        get() = _tvShowsLiveData
+
+    val onRefreshListener = SwipeRefreshLayout.OnRefreshListener {
+        getTvShows()
+    }
+
+    init {
+        getTvShows()
+    }
+
+    private fun getTvShows() {
+        viewModelScope.launch {
+            interactors.getAllTvShows.invoke().collect {
+                _tvShowsLiveData.postValue(it)
+            }
+        }
+    }
 }
