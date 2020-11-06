6abed5a20227996c966b6c3dceb799b4e41d3828
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-11-06 09:47:16 +0700

    Create MovieRoutes.kt

---
+++ app/src/main/java/com/laam/moviedb_cleanarch/framework/data/network/routes/MovieRoutes.kt
@@ -0,0 +1,20 @@
+package com.laam.moviedb_cleanarch.framework.data.network.routes
+
+import com.laam.core.model.MoviePagination
+import com.laam.moviedb_cleanarch.BuildConfig
+import com.laam.moviedb_cleanarch.framework.data.network.result.MovieDetailResult
+import com.laam.moviedb_cleanarch.framework.data.network.result.MovieResult
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface MovieRoutes {
+
+    @GET("movie/popular?region=${BuildConfig.MOVIE_DB_REGION}")
+    suspend fun getMoviesPopular(): Response<MoviePagination<MovieResult>>
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovie(
+        @Path("movie_id") movieId: Long
+    ): Response<MovieDetailResult>
+}
