f90160f3f266c01e03e62c08abd983f9d068ddc3
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-11-06 09:50:13 +0700

    Create NetworkModule.kt

--- app/src/main/java/com/laam/moviedb_cleanarch/framework/di/component/AppComponent.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/framework/di/component/AppComponent.kt
@@ -2,6 +2,7 @@ package com.laam.moviedb_cleanarch.framework.di.component
 
 import android.app.Application
 import com.laam.moviedb_cleanarch.framework.di.builder.ActivityBuilder
+import com.laam.moviedb_cleanarch.framework.di.module.NetworkModule
 import com.laam.moviedb_cleanarch.framework.di.module.RepositoryModule
 import com.laam.moviedb_cleanarch.framework.di.module.ViewModelModule
 import com.laam.moviedb_cleanarch.framework.di.module.ViewModelProviderFactoryModule
@@ -19,7 +20,8 @@ import javax.inject.Singleton
         ActivityBuilder::class,
         ViewModelProviderFactoryModule::class,
         ViewModelModule::class,
-        RepositoryModule::class
+        RepositoryModule::class,
+        NetworkModule::class
     ]
 )
 interface AppComponent : AndroidInjector<MovieApplication> {
