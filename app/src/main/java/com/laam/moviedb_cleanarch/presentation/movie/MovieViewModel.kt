92c5a3b4b65a3bbc7dfe9c8dd9cd3b2ecfc63737
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-11-06 09:56:24 +0700

    Reconfigure MovieFragment to get data from API and add loading state

--- app/src/main/java/com/laam/moviedb_cleanarch/presentation/movie/MovieViewModel.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/presentation/movie/MovieViewModel.kt
@@ -1,13 +1,43 @@
 package com.laam.moviedb_cleanarch.presentation.movie
 
 import androidx.databinding.ObservableBoolean
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.laam.core.ext.repository.State
+import com.laam.core.model.Movie
+import com.laam.core.model.MoviePagination
 import com.laam.moviedb_cleanarch.presentation.base.BaseViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MovieViewModel @Inject constructor(
-    interactors: MovieListInteractors
+    private val interactors: MovieListInteractors
 ) : BaseViewModel() {
 
+    val isLoading: ObservableBoolean = ObservableBoolean(false)
     val isEmptyData: ObservableBoolean = ObservableBoolean(false)
-    val movieList = interactors.getAllMovies.invoke()
+
+    private val _moviesLiveData = MutableLiveData<State<MoviePagination<Movie>>>()
+    val moviesLiveData: LiveData<State<MoviePagination<Movie>>>
+        get() = _moviesLiveData
+
+    val onRefreshListener = SwipeRefreshLayout.OnRefreshListener {
+        getMovies()
+    }
+
+    init {
+        getMovies()
+    }
+
+    private fun getMovies() {
+        viewModelScope.launch {
+            interactors.getAllMovies.invoke().collect {
+                _moviesLiveData.postValue(it)
+            }
+        }
+    }
+
 }
