cc40acd14b6a0f93febe90100e3f5d677015c8b9
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-11-13 06:39:55 +0700

    Add `getViewModelScope` in BaseViewModel.kt to get viewmodel scope

--- app/src/main/java/com/laam/moviedb_cleanarch/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/laam/moviedb_cleanarch/presentation/base/BaseViewModel.kt
@@ -1,5 +1,12 @@
 package com.laam.moviedb_cleanarch.presentation.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
 
-abstract class BaseViewModel : ViewModel()
+abstract class BaseViewModel(
+    private val coroutineScope: CoroutineScope? = null
+) : ViewModel() {
+
+    fun getViewModelScope() = coroutineScope ?: this.viewModelScope
+}
