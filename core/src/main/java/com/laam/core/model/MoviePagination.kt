b54d805325f4818bdec4a3f28817c718b15dab7d
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-11-13 06:38:49 +0700

    Add default value for MoviePagination class

--- core/src/main/java/com/laam/core/model/MoviePagination.kt
+++ core/src/main/java/com/laam/core/model/MoviePagination.kt
@@ -1,8 +1,8 @@
 package com.laam.core.model
 
 data class MoviePagination<T>(
-    val page: Int,
-    val total_pages: Int,
-    val total_results: Int,
+    val page: Int = 0,
+    val total_pages: Int = 0,
+    val total_results: Int = 0,
     val results: List<T>
 )
