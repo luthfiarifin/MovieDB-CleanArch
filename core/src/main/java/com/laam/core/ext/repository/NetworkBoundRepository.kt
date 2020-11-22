69db0d9cd7e8c418de8c57f4cfe3249560130543
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-11-22 23:37:27 +0700

    Update NetworkBoundRepository to support cache with pagination

--- core/src/main/java/com/laam/core/ext/repository/NetworkBoundRepository.kt
+++ core/src/main/java/com/laam/core/ext/repository/NetworkBoundRepository.kt
@@ -3,33 +3,44 @@ package com.laam.core.ext.repository
 import com.laam.core.ext.idling.EspressoIdlingResource
 import kotlinx.coroutines.flow.flow
 import retrofit2.Response
-import java.lang.Exception
 
-abstract class NetworkBoundRepository<REQUEST, RESPONSE> {
+abstract class NetworkBoundRepository<DB, REMOTE> {
 
-    fun asFlow() = flow<State<RESPONSE>> {
+    fun asFlow() = flow<State<DB>> {
         EspressoIdlingResource.increment()
 
         emit(State.loading())
+
+        val localData = fetchFromLocal()
 
         try {
             val apiResponse = fetchFromRemote()
             val remoteData = apiResponse.body()
 
             if (apiResponse.isSuccessful && remoteData != null) {
+                if (shouldSaveToLocal(remoteData)) {
+                    saveRemoteData(remoteData)
+                }
+
                 emit(State.success(mapFromRemote(remoteData)))
             } else {
-                emit(State.error(apiResponse.message()))
+                emit(State.error(apiResponse.message(), localData))
             }
 
             EspressoIdlingResource.decrement()
         } catch (ex: Exception) {
-            emit(State.error("Network error!"))
+            emit(State.error("Cannot fetch from remote, network error.", localData))
             ex.printStackTrace()
         }
     }
 
-    protected abstract suspend fun fetchFromRemote(): Response<REQUEST>
+    protected abstract suspend fun fetchFromRemote(): Response<REMOTE>
 
-    protected abstract suspend fun mapFromRemote(request: REQUEST): RESPONSE
+    protected abstract fun fetchFromLocal(): DB
+
+    protected abstract fun saveRemoteData(data: REMOTE)
+
+    protected abstract fun shouldSaveToLocal(data: REMOTE?): Boolean
+
+    protected abstract fun mapFromRemote(data: REMOTE): DB
 }
--- core/src/main/java/com/laam/core/ext/repository/State.kt
+++ core/src/main/java/com/laam/core/ext/repository/State.kt
@@ -5,7 +5,7 @@ sealed class State<T> {
 
     data class Success<T>(val data: T) : State<T>()
 
-    data class Error<T>(val message: String) : State<T>()
+    data class Error<T>(val message: String, val data: T?) : State<T>()
 
     companion object {
 
@@ -13,6 +13,6 @@ sealed class State<T> {
 
         fun <T> success(data: T) = Success(data)
 
-        fun <T> error(message: String) = Error<T>(message)
+        fun <T> error(message: String, data: T?) = Error<T>(message, data)
     }
 }
