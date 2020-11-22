6e189b98757d3d9cfd13496cdd4e10f10fbcbc5f
Luthfi Arifin <luthfiarifin0222@gmail.com>
2020-11-22 23:39:55 +0700

    Change repository return

--- core/src/main/java/com/laam/core/repository/MovieRepository.kt
+++ core/src/main/java/com/laam/core/repository/MovieRepository.kt
@@ -2,12 +2,11 @@ package com.laam.core.repository
 
 import com.laam.core.ext.repository.State
 import com.laam.core.model.MovieEntity
-import com.laam.core.model.MoviePagination
 import kotlinx.coroutines.flow.Flow
 
 interface MovieRepository {
 
-    suspend fun getAll(): Flow<State<MoviePagination<MovieEntity>>>
+    suspend fun getAll(page: Int): Flow<State<Pair<Int, List<MovieEntity>>>>
 
     suspend fun get(id: Long): Flow<State<MovieEntity?>>
 }
